b0d5ccfb1205428c27fc52fed7098759ad4210a9
Merwan Achibet <merwandonut@hotmail.com>
2012-04-30 14:13:59 +0200

    Add static class to load shapefiles
    From the shapefile are loaded the centroids of each land lots in order
    to later generate the Voronoi diagram. The loading process is
    extremely crude and I'm pretty sure it won't work with more exotic
    shapefiles.

--- makefile
+++ makefile
@@ -6,9 +6,17 @@ BUILD_DIR=build
 LIB_DIR=lib
 
 LIBS=\
-	gs-core-1.2-git-last \
+	geoapi-2.2-M1 \
 	gs-algo-1.2-git-last \
+	gs-core-1.2-git-last \
 	gs-ui-1.2-git-last \
+	gt-api-2.5.1 \
+	gt-main-2.5.1 \
+	gt-metadata-2.5.1 \
+	gt-epsg-hsql-2.5.1 \
+	gt-referencing-2.5.1 \
+	gt-shapefile-2.5.1 \
+	jsr-275-1.0-beta-2 \
 	jts-1.12
 
 FILES=\
@@ -22,6 +30,7 @@ FILES=\
 	MouseManager \
 	RandomDensityStrategy \
 	RoadOps \
+	ShapeFileLoader \
 	Simulation \
 	Test
 
@@ -29,10 +38,12 @@ CP:= $(addprefix $(LIB_DIR)/,$(LIBS))
 CP:= $(addsuffix .jar,$(CP))
 CP:= $(subst $(SPACE),:,$(CP))
 
+OPTIONS=
+
 all: compile run
 
 compile: $(addprefix $(SRC_DIR)/,$(addsuffix .java,$(FILES)))
-	javac -cp $(CP) -d $(BUILD_DIR) $^
+	javac -cp $(CP) -d $(BUILD_DIR) $(OPTIONS) $^
 
 run:
 	cd $(BUILD_DIR) && java -cp .:$(subst lib/,../lib/,$(CP)) Test
---
+++ src/ShapeFileLoader.java
@@ -0,0 +1,68 @@
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+
+import org.opengis.feature.Feature;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.geometry.Geometry;
+
+final class ShapeFileLoader {
+
+	 public static Coordinate[] getLandLots(String fileName) {
+
+		  ArrayList<Point> points = new ArrayList<Point>();
+
+		  try {
+
+				// Load the shapefile.
+				File file = new File(fileName);
+				if(file == null) {
+					 System.err.println("Impossible to load file " + fileName);
+					 return null;
+				}
+
+				Map<String, URL> connect = new HashMap<String, URL>();
+				connect.put("url", file.toURL());
+
+				DataStore store = DataStoreFinder.getDataStore(connect);
+				String[] typeNames = store.getTypeNames();
+				String typeName = typeNames[0];
+
+				FeatureCollection features = store.getFeatureSource(typeName).getFeatures();
+
+				// Go through each and every polygon.
+				FeatureIterator iterator = features.features();
+				while(iterator.hasNext()) {
+
+					 SimpleFeature f = (SimpleFeature)iterator.next();
+
+					 Point centroid = ((MultiPolygon)f.getDefaultGeometry()).getCentroid();
+
+					 points.add(centroid);
+				}
+
+		  }
+		  catch(Exception e) {
+				e.printStackTrace();
+		  }
+
+		  // From ArrayList of Point to array of Coordinate.
+		  Coordinate[] coords = new Coordinate[points.size()];
+		  for(int i = 0, l = points.size(); i < l; ++i)
+				coords[i] = points.get(i).getCoordinate();
+
+		  return coords;
+	 }
+}
